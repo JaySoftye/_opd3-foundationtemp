97862a627d12c9ed586d241a49b384758b676bd7
JaySoftye <JaySoftye@iMac.local>
2017-08-03 15:32:31 -0400

    Login Window and Password Change Update
    style updates to the password change process

--- login-window.php
+++ login-window.php
@@ -1,49 +1,128 @@
 <div class="modal fade login-window" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
 
-      <section class="modal-body light-gray-bg padding">
-        <p class="text-center"><img src="assets/opd_icon.svg" width="80" height="42" alt="" /> </p>
-        <form name="" method="">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Email">
-            <input type="text" class="form-control" placeholder="Password">
-          </div>
-          <button type="submit" class="login-button">Login</button>
-        </form>
 
-        <p class="text-center"><small><a href="#" data-toggle="modal" data-target=".password-reset">Forgot Password</a> | <a href="#">Register Group Code</a></small></p>
-        <h2 class="text-center"><strong>- or -</strong></h2>
-        <button type="submit" class="google-login">Login with your Google Account</button>
-      </section>
-
-      <section class="modal-body">
-        <p class="text-center">Not a member yet? Find a pricing plan that works for you.</p>
-        <div class="row">
-          <a href="#" class="col text-center">
-            <img src="assets/login-group-pricing-icon.svg" alt="" />
-            <h4 class="pd-blue-text"><strong>Group Pricing</strong></h4>
-            <p><small class="pd-blue-text">School and District Pricing Plans</small></p>
-          </a>
-          <a href="#" class="col text-center">
-            <img src="assets/login-single-pricing-icon.svg" alt="" />
-            <h4 class="green-text"><strong>Single User Pricing</strong></h4>
-            <p><small class="green-text">Individual Monthly and Yearly Plans</small></p>
-          </a>
-          <a href="#" class="col text-center">
-            <img src="assets/login-basic-pricing-icon.svg" alt="" />
-            <h4 class="lavendar-text"><strong>Basic User Account</strong></h4>
-            <p><small class="lavendar-text">Sign Up For a Basic User Account</small></p>
-          </a>
-        </div>
-      </section>
-
-      <div class="modal-footer">
-        <button type="button" class="close float-right" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-
+    <div class="modal-content" id="login-form">
+      <div class="modal-body">
+        <section class="container light-gray-bg padding">
+          <p class="text-center"><img src="assets/opd_icon.svg" width="80" height="42" alt="" /></p>
+          <form name="" method="" class="login-password-form">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Email">
+              <input type="text" class="form-control" placeholder="Password">
+            </div>
+            <button type="submit" class="login-button">Login</button>
+          </form>
+          <p class="text-center"><small><a href="#" onclick="showHide()">Forgot Password</a> | <a href="#">Register Group Code</a></small></p>
+          <h3 class="text-center"><strong>- or -</strong></h3>
+          <button type="submit" class="google-login">Login with your Google Account</button>
+        </section>
       </div>
+      <div class="modal-footer">
+        <section class="container padding">
+          <p class="text-center">Don't have a Teq Online PD account yet?<br /><strong>Find a pricing plan that works for you.</strong></p>
+          <div class="row">
+            <a href="#" class="col text-center">
+              <img src="assets/login-group-pricing-icon.svg" alt="" />
+              <h4 class="pd-blue-text"><strong>Group Pricing</strong></h4>
+              <small class="pd-blue-text">School and District Pricing Plans</small>
+            </a>
+            <a href="#" class="col text-center">
+              <img src="assets/login-single-pricing-icon.svg" alt="" />
+              <h4 class="green-text"><strong>Single User Pricing</strong></h4>
+              <small class="green-text">Individual Monthly and Yearly Plans</small>
+            </a>
+            <a href="#" class="col text-center">
+              <img src="assets/login-basic-pricing-icon.svg" alt="" />
+              <h4 class="lavendar-text"><strong>Basic User Account</strong></h4>
+              <small class="lavendar-text">Sign Up For a Basic User Account</small>
+            </a>
+          </div>
+        </section>
+      </div>
+    </div>
+
+    <div class="modal-content" id="password-reset-form" style="display: none;">
+      <div class="modal-body">
+        <section class="container light-gray-bg padding">
+          <h3 class="text-center"><strong>Forgot Your Password</strong></h3>
+          <p class="text-center">Please enter the email address associated with your account and follow the instructions to reset your password.</p>
+          <form name="" method="">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Email">
+            </div>
+            <button type="submit" class="login-button" onclick="showQues()">Enter</button>
+          </form>
+        </section>
+      </div>
+      <div class="modal-footer">
+        <section class="container">
+          <a href="#"><small class="blue-text">Not a member? <u>Click here</u></small></a>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </section>
+      </div>
+    </div>
+
+    <div class="modal-content" id="security-ques-form" style="display: none;">
+      <div class="modal-body">
+        <section class="container light-gray-bg padding">
+          <h5 class="text-center padding-bottom"><strong>Please answer the following security questions to reset your password.</strong></h5>
+          <form name="" method="">
+            <p class="text-center">What color was your first car?</<p>
+            <div class="form-group">
+              <input type="text" class="form-control">
+            </div>
+            <p class="text-center">What was the street you grew up on?</<p>
+            <div class="form-group">
+              <input type="text" class="form-control">
+            </div>
+            <button type="submit" class="login-button">Submit Answers</button>
+          </form>
+        </section>
+      </div>
+      <div class="modal-footer">
+        <section class="container">
+          <a href="#"><small class="blue-text">Not a member? <u>Click here</u></small></a>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </section>
+      </div>
+    </div>
+
+      <script>
+      /**
+      DELETE THIS
+      ONLY USED TO SHOW HOW THE FORM WILL WORK ON THE FRONT END
+      **/
+        function showHide() {
+          var x = document.getElementById('login-form');
+          var y = document.getElementById('password-reset-form');
+
+          if (y.style.display === 'none') {
+            x.style.display = 'none';
+            y.style.display = 'block';
+          } else {
+            y.style.display = 'none';
+            }
+          }
+
+          function showQues() {
+            var a = document.getElementById('security-ques-form');
+            var b = document.getElementById('password-reset-form');
+
+            if (a.style.display === 'none') {
+              b.style.display = 'none';
+              a.style.display = 'block';
+            } else {
+              a.style.display = 'none';
+              }
+            }
+        </script>
+
+
 
     </div>
   </div>
