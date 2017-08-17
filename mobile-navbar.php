96ed092b5ed54aa747d5d38a451c0ad0b722819f
JaySoftye <JaySoftye@iMac.local>
2017-08-17 15:19:09 -0400

    Mobile Navigation Fix
    Style and now chat box

--- mobile-navbar.php
+++ mobile-navbar.php
@@ -1,57 +1,111 @@
 <nav class="container-fluid hidden-md-up" id="mobile-navbar">
-  <div class="row" id="mainBanner">
 
-    <div class="col-8">
-      <a data-toggle="collapse" href="#mobile-main-nav" aria-expanded="false" aria-controls="mobile-main-nav"><img src="assets/mobile_navbar_opd_logo.svg" alt="Online Professional Development the fusion of technology and learning" /></a>
+  <section class="container" id="mainBanner">
+    <div class="row">
+      <div class="col-8 no-padding">
+        <a data-toggle="collapse" href="#mobile-main-nav" aria-expanded="false" aria-controls="mobile-main-nav"><img src="assets/mobile_navbar_opd_logo.svg" alt="Online Professional Development the fusion of technology and learning" /></a>
+      </div>
+      <div class="col-4 no-padding">
+        <a data-toggle="collapse" href="#mobile-now-window" aria-expanded="false" aria-controls="mobile-now-window"><img src="assets/mobile_navbar_now_logo.svg" alt="now instructional support" /></a>
+      </div>
     </div>
-    <div class="col-4">
-      <a href="#"><img src="assets/mobile_navbar_now_logo.svg" alt="now instructional support" /></a>
+  </section>
+
+  <section class="collapse container" id="mobile-main-nav">
+    <div class="row">
+      <div class="col">
+        <button type="button" class="close float-left" data-toggle="collapse" href="#mobile-main-nav" aria-expanded="false" aria-controls="mobile-main-nav">
+          <span class="display-3 white">&times;</span>
+        </button>
+      </div>
     </div>
+    <div class="row">
+      <div class="col">
+        <ul class="nav flex-column">
+          <li class="nav-item hidden">
+            <a class="nav-link" href="#" data-toggle="modal" data-target=".login-window">Login</a>
+          </li>
+          <li class="nav-item">
+            <section class="align-items-center active">
+              <div class="row"><a href="#" id="profile-image"><span>UN</span></a></div>
+            </section>
+            <a class="nav-link" href="#">Log Out</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link dropdown-toggle" href="" id="profile-mobilenav-option" data-toggle="dropdown" aria-expanded="false">My Profile</a>
+            <div class="mobile-sub-menu dropdown-menu" aria-labelledby="profile-mobilenav-option">
+              <a href="#">PD Plan</a>
+              <a href="#">My Usage</a>
+              <a href="#">My Account</a>
+              <a href="#">My Reports</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link dropdown-toggle" href="" id="calendar-mobilenav-option" data-toggle="dropdown" aria-expanded="false">Course Calendar</a>
+            <div class="mobile-sub-menu dropdown-menu" aria-labelledby="calendar-mobilenav-option">
+              <a href="#">All</a>
+              <a href="#">Teachers</a>
+              <a href="#">Administrators</a>
+              <a href="#">Parents</a>
+              <a href="#">Free</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link dropdown-toggle" href="" id="library-mobilenav-option" data-toggle="dropdown" aria-expanded="false">Course Library</a>
+            <div class="mobile-sub-menu dropdown-menu" aria-labelledby="library-mobilenav-option">
+              <a href="#">All</a>
+              <a href="#">Teachers</a>
+              <a href="#">Administrators</a>
+              <a href="#">Parents</a>
+              <a href="#">Free</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link dropdown-toggle" href="" id="library-mobilenav-option" data-toggle="dropdown" aria-expanded="false">Help Center</a>
+            <div class="mobile-sub-menu dropdown-menu" aria-labelledby="library-mobilenav-option">
+              <a href="#">How-to-videos</a>
+              <a href="#">Skills and Techniques</a>
+              <a href="#">Lesons</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </section>
 
-
-    <section class="collapse container" id="mobile-main-nav">
+    <section class="collapse container" id="mobile-now-window">
       <div class="row">
-        <div class="col-8">
-          <a data-toggle="collapse" href="#mobile-main-nav" aria-expanded="false" aria-controls="mobile-main-nav"><img src="assets/mobile_navbar_opd_logo.svg" alt="Online Professional Development the fusion of technology and learning" /></a>
-        </div>
-        <div class="col-4">
-          <button type="button" class="close" data-toggle="collapse" href="#mobile-main-nav" aria-expanded="false" aria-controls="mobile-main-nav">
-            <span>&times;</span>
+        <div class="col">
+          <button type="button" class="close float-left" data-toggle="collapse" href="#mobile-now-window" aria-expanded="false" aria-controls="mobile-now-window">
+            <span class="display-3 white">&times;</span>
           </button>
         </div>
       </div>
-        <div class="row">
-          <div class="col">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link dropdown-toggle" href="" id="profile-mobilenav-option" data-toggle="dropdown" aria-expanded="false">My Profile</a>
-                <div class="mobile-sub-menu dropdown-menu" aria-labelledby="profile-mobilenav-option">
-                  <section class="container">
-                    <ul class="nav flex-column">
-                      <li>
-                        <a href="#">How-to-videos</a>
-                      </li>
-                      <li>
-                        <a href="#">Skills and Techniques</a>
-                      </li>
-                      <li>
-                        <a href="#">Lessons</a>
-                      </li>
-                    </ul>
-                  </section>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-            </ul>
-          </div>
+      <div class="row align-items-start" id="now-chat-box">
+        <div class="col-8">
+          <p class="white">Chat nOw or connect via one of our other options from the menu below</p>
         </div>
+        <div class="col-4">
+          <img src="assets/mobile_navbar_now_logo.svg" alt="" />
+        </div>
+          <form name="" method="" id="chat-box-conversation">
+            <input type="text" placeholder="type here">
+          </form>
+      </div>
+      <div class="row" id="now-chat-window">
+          <div class="col">
+            <a href="#"><img src="assets/now-instructional-support-live-video.svg" alt="Live Video" /></a>
+          </div>
+          <div class="col">
+            <a href="#"><img src="assets/now-instructional-support-schedule-call.svg" alt="Schedule a Call" /></a>
+          </div>
+          <div class="col">
+            <a href="#"><img src="assets/now-instructional-support-email-us.svg" alt="Email Us" /></a>
+          </div>
       </div>
     </section>
 
-  </div>
 </nav>
