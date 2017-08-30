e68a6e9796ad41d72e45e28be05a6e570b2cdfea
JaySoftye <JaySoftye@iMac.local>
2017-08-30 16:53:32 -0400

    Empty PD Plan Recommendations Page

---
+++ profile-pd-plan-empty.php
@@ -0,0 +1,54 @@
+<?php include 'header.php'; ?>
+  <?php include 'now-chat-window.php'; ?>
+
+  <main>
+
+    <section class="container-fluid under-header light-blue-bg">
+      <div class="container">
+
+        <div class="row">
+          <div class="col">
+            <ul class="nav profile-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">My PD Plan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">My Usage</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">My Account</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">My Reports</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="container-fluid advantageHuman">
+      <div class="container">
+        <div class="row text-center padding-top padding-bottom">
+          <div class="col padding-top">
+            <h1 class="teq-blue-text"><strong>We're sorry.</strong></h1>
+            <p>There appears to be no current recommendations for you at this time.<br /><small>Have a question for us? Contact us directly at <a href="mailto:onlinepd@teq.com"><u>OnlinePD@teq.com</u></a></small></p>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col">
+            <h4>Try checking out the <a href="http://opd-dev.teq.com/events">Course Calendar</a> for all our new and upcoming courses!</h4>
+          </div>
+        </div>
+        <div class="row text-center padding-top padding-bottom">
+          <div class="col padding-bottom">
+            <a class="lg-btn gray-fill" href="http://opd-dev.teq.com/events">Take me to the calendar</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+<?php include 'footer.php'; ?>
