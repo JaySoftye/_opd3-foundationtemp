be14f3362242c0dfb2330dff9aec5fe74f9e093c
JaySoftye <JaySoftye@iMac.local>
2017-09-11 11:40:09 -0400

    nOw Chat Window update

--- now-chat-window.php
+++ now-chat-window.php
@@ -10,9 +10,17 @@
   <button type="button" class="close" id="now-chat-close">
     <img src="assets/close-icon.svg" alt="close" />
   </button>
+  <div class="mobile-now-chat-window">
+    <div class="col">
+      <p class="white">Chat nOw or connect via one of our other options from the menu below</p>
+    </div>
+  </div>
   <div class="row">
-    <div class="col-4">
-      <img src="assets/now-instructional-support-chat-via.svg" alt="Chat nOw or connect via one of our other options" />
+    <div class="col-4 hide-small">
+      <a href="#" class="active"><img src="assets/now-instructional-support-chat-via.svg" alt="Chat nOw or connect via one of our other options" /></a>
+    </div>
+    <div class="col mobile-now-chat-window">
+      <a href="#" class="active"><img src="assets/now-instructional-support-chat-now.svg" alt="Chat nOw" /></a>
     </div>
     <div class="col">
       <a href="#"><img src="assets/now-instructional-support-live-video.svg" alt="Live Video" /></a>
@@ -23,7 +31,7 @@
     <div class="col">
       <a href="#"><img src="assets/now-instructional-support-email-us.svg" alt="Email Us" /></a>
     </div>
-    <div class="col">
+    <div class="col hide-small">
       <img src="assets/now-instructional-support-chat-icon.svg" alt="" />
     </div>
   </div>
