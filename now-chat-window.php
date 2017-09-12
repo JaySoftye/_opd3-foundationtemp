8ffeaf0c2c62cb9ecb53e7922106d2bbbee7c1a5
JaySoftye <JaySoftye@iMac.local>
2017-09-12 09:47:09 -0400

    Chat Window Fix

--- now-chat-window.php
+++ now-chat-window.php
@@ -1,7 +1,7 @@
 <div id="now-instructional-support">
   <div class="row">
     <div class="col-4">
-      <img src="assets/now-instructional-support-chat-window.svg" alt="nOw Instructional Support" />
+      <a href="javascript:void(0);" onclick="olark('api.box.expand')"><img src="assets/now-instructional-support-chat-window.svg" alt="nOw Instructional Support" /></a>
     </div>
   </div>
 </div>
@@ -42,9 +42,26 @@
   </div>
   <div class="row" id="now-chat-box">
     <div class="col">
-      <form name="" method="" id="chat-box-conversation">
-        <input type="text" placeholder="type here">
-      </form>
+      <div id="olark-box-container" style="overflow: auto;"></div>
+      <!-- begin olark code -->
+      <script type="text/javascript" async>
+      ;(function(o,l,a,r,k,y){if(o.olark)return;
+      r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+      y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+      y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+      y.extend=function(i,j){y("extend",i,j)};
+      y.identify=function(i){y("identify",k.i=i)};
+      y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+      k=y._={s:[],t:[+new Date],c:{},l:a};
+      })(window,document,"static.olark.com/jsclient/loader.js");
+
+      function startOlark() {
+        olark('api.box.expand');
+      }
+      olark.configure('box.inline', true);
+      olark.configure('system.hb_chatbox_size', 'sm');
+      olark.identify('2098-857-10-1001');</script>
+      <!-- end olark code -->
     </div>
   </div>
 </aside>
