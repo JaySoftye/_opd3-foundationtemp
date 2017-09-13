e7df3e2a55d14b2b18450687ae7c4c96e35d6c40
JaySoftye <JaySoftye@iMac.local>
2017-09-13 10:51:52 -0400

    Footer Change
    NOT for launch

---
+++ certification-footer.php
@@ -0,0 +1,293 @@
+<?php include 'header.php'; ?>
+  <?php include 'now-chat-window.php'; ?>
+
+  <main>
+
+    <section class="container-fluid edTech">
+      <div class="row align-items-center">
+        <div class="container">
+          <div class="col-md-7 col-sm-10">
+            <h1><img src="assets/your-edtech-skills-will-skyrocket.svg" alt="Your Ed Tech Skills will Skyrocket" /></h1>
+            <h4 class="white">Boost your classroom technology skills to <strong>engage your students and improve instruction</strong> with Teq’s Online Professional Development platform.</h4>
+            <div class="padding-top">
+              <h5 class="white"><strong>Start a </strong> <a class="lg-btn white-border" href="#">Free Basic Account</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container padding-top padding-bottom">
+      <div class="row">
+        <div class="col-3">
+        </div>
+        <div class="col-6 padding-top">
+          <h3><img src="assets/learn-anywhere-text.svg" alt="Learn Anywhere" /></h3>
+        </div>
+        <div class="col-3">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-sm-6 text-center">
+          <img src="assets/learn-anywhere-teacher-image.jpg" alt="" />
+          <h2 class="blue-text">Teachers</h2>
+          <p><strong>Fulfill state-required</strong> PD hours, master that new piece of technology, or simply fine-tune your skills. Learn when you want and receive instant help when you need it with Teq Online PD.</p>
+        </div>
+        <div class="col-md-3 col-sm-6 text-center">
+          <img src="assets/learn-anywhere-administrators-image.jpg" alt="" />
+          <h2 class="blue-text">Administrators</h2>
+          <p>Align PD with your instructional goals. <strong>Create personalized PD plans, customize course quizzes, receive robust analytics,</strong> and gain access to on-demand support and pd resources.</p>
+        </div>
+        <div class="col-md-3 col-sm-6 text-center">
+          <img src="assets/learn-anywhere-parents-image.jpg" alt="" />
+          <h2 class="blue-text">Parents</h2>
+          <p>As parents raising 21st century students, you need answers too! With insight into College and <strong>Career Readiness, Digital Citizenship, Social Media, Gamification, Understanding Common Core</strong> (and much much more) you will feel confident supporting your young learners.</p>
+        </div>
+        <div class="col-md-3 col-sm-6 text-center">
+          <img src="assets/learn-anywhere-free-image.jpg" alt="" />
+          <h2 class="blue-text">Free</h2>
+          <p>It’s on us! Start a Free Basic Account and <strong>receive complimentary access to select courses</strong>, instructional videos to help you get started using today’s top classroom technologies.</p>
+        </div>
+      </div>
+      <div class="row padding-bottom">
+        <div class="col-md-4 col-sm-3">
+        </div>
+        <div class="col-md-4 col-sm-6 text-center padding-top">
+          <a class="lg-btn blue-border" href="#">Sign Up</a>
+        </div>
+        <div class="col-md-4 col-sm-3">
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid padding-top padding-bottom whatYouGet">
+      <div class="container">
+        <div class="row">
+          <div class="col-3"></div>
+          <div class="col-6 padding-top">
+            <h3><img src="assets/what-you-get-text.svg" alt="What you get" /></h3>
+          </div>
+          <div class="col-3"></div>
+        </div>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 text-center">
+            <img src="assets/what-you-get-certifications-icon.png" alt="" />
+            <h2 class="white">Certifications</h2>
+            <p>Comprised of in-depth, chaptered sessions that guide you through the complete implementation of popular classroom technology. Current <strong>certification tracks include G Suite (Google), SMART, Apple, Microsoft</strong>, and more.</p>
+          </div>
+          <div class="col-md-3 col-sm-6 text-center">
+            <img src="assets/what-you-get-state-approved-icon.png" alt="" />
+            <h2 class="white">State-Approved</h2>
+            <p>As a <strong>state-approved PD provider,</strong> Teq Online PD is a convenient and flexible way to fulfill your required PD hours from the comfort of your home.</p>
+            <p>Visit <a href="http://www.teq.com/state-approved" class="white"><strong>www.teq.com/State-Approved</strong></a> for a complete list of State Approved Professional Development.</p>
+          </div>
+          <div class="col-md-3 col-sm-6 text-center">
+            <img src="assets/what-you-get-analytics-icon.png" alt="" />
+            <h2 class="white">Analytics</h2>
+            <p>Track how your school is progressing towards your PD goals. <strong>See what courses and certifications are being completed by teachers</strong> to guide your PD initiatives. Teachers can earn badges and track their individual success.</p>
+          </div>
+          <div class="col-md-3 col-sm-6 text-center">
+            <img src="assets/what-you-get-live-courses-icon.png" alt="" />
+            <h2 class="white">Live Courses</h2>
+            <p>Live courses each week on how to incorporate classroom technologies into instruction. <strong>Interact directly with our presenters</strong>, ask questions, and master new ed tech skills.</p>
+          </div>
+        </div>
+        <div class="row padding-bottom">
+          <div class="col-md-4 col-sm-3"></div>
+          <div class="col-md-4 col-sm-6 text-center padding-top">
+            <a class="lg-btn white-border" href="#">Sign Up</a>
+          </div>
+          <div class="col-md-4 col-sm-3"></div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid padding-top padding-bottom light-blue-bg">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-4">
+            <p class="blue-text"><span class="stat-text">20,567</span>Courses Taken</p>
+          </div>
+          <div class="col-md-4">
+            <p class="blue-text"><span class="stat-text">1,005</span>Schools on the Platform</p>
+          </div>
+          <div class="col-md-4">
+            <p class="blue-text"><span class="stat-text">20,567</span>Teachers on the Plaform</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid advantageHuman">
+      <div class="container">
+
+          <div class="row text-center padding-top">
+            <div class="col-md-3"></div>
+            <div class="col-md-6 padding-top">
+              <h3><img src="assets/advantage-human-text.svg" alt="Advantage Human" /></h3>
+            </div>
+            <div class="col-md-3"></div>
+          </div>
+
+          <div class="row text-center">
+            <div class="col">
+              <h4><strong>As a subscriber</strong>, you receive access to “getting started” videos, and our <strong>nOw Instructional Support</strong>, where <strong>you can interact directly</strong> with our team of Professional Development Specialists.</h4>
+            </div>
+          </div>
+
+          <div class="row align-items-center">
+            <div class="col-md-5 col-xs-4 text-right">
+              <p>Meet <span class="teq-blue-text">Emily Jones</span><br />One of our On-Demand<br />Professional Development Specialists</p>
+            </div>
+            <div class="col-md-2 col-xs-4">
+              <a data-toggle="modal" data-target=".now-instructor-vid" title="view profile"><img src="assets/now-specialist-emily-jones-btn.png" alt="" /></a>
+            </div>
+            <div class="col-md-5 col-xs-4"></div>
+          </div>
+
+          <div class="modal fade now-instructor-vid" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">nOw Instructional Support Specialist</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <video controls class="embed-responsive-item">
+                      <source src="" type="video/mp4">
+                      <source src="" type="video/ogg">
+                      Your browser does not support HTML5 video.
+                    </video>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row text-center padding-bottom">
+            <div class="col padding-bottom margin-top">
+              <a class="lg-btn gray-fill" href="#">Sign Up</a>
+            </div>
+          </div>
+
+      </div>
+    </section>
+
+    <section class="container-fluid padding-top padding-bottom">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-3"></div>
+          <div class="col-6 text-center padding-top">
+            <h3><img src="assets/what-we-do-text.svg" alt="What we do" /></h3>
+          </div>
+          <div class="col-3"></div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 hide-small no-padding">
+          <img src="assets/what-we-do-online-pd-image.jpg" alt="" />
+        </div>
+        <div class="col-md-3 text-center padding-bottom">
+
+          <a data-toggle="modal" data-target=".what-we-do-opd-vid" title="Online PD Video"><img src="assets/what-we-do-online-pd-vid-btn.jpg" alt="" /></a>
+          <a href="" data-toggle="modal" data-target=".what-we-do-opd-vid" class="lg-btn blue-border">Play Video</a>
+          <div class="modal fade what-we-do-opd-vid" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">What we do</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <video controls class="embed-responsive-item">
+                      <source src="" type="video/mp4">
+                      <source src="" type="video/ogg">
+                      Your browser does not support HTML5 video.
+                    </video>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-3 text-center padding-bottom">
+
+          <a data-toggle="modal" data-target=".what-we-do-testimonial-vid" title="Case Study Video"><img src="assets/what-we-do-getting-started-vid-btn.jpg" alt="" /></a>
+          <a href="" data-toggle="modal" data-target=".what-we-do-testimonial-vid" class="lg-btn gray-border">Play Video</a>
+          <div class="modal fade what-we-do-testimonial-vid" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Customer Success</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="embed-responsive embed-responsive-16by9">
+                    <video controls class="embed-responsive-item">
+                      <source src="" type="video/mp4">
+                      <source src="" type="video/ogg">
+                      Your browser does not support HTML5 video.
+                    </video>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-md-3 hide-small no-padding">
+          <img src="assets/what-we-do-getting-started-image.jpg" alt="" />
+        </div>
+      </div>
+    </section>
+
+    <section class="container-fluid padding-top padding-bottom light-gray-bg">
+      <div class="container">
+
+        <div class="row text-center padding-top">
+          <div class="col">
+            <h2>Popular Topics</h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+            <img src="assets/popular-topics-google-icon.jpg" alt="" />
+            <p><strong>Google for Education</strong>. Our specialists can help with learning key strategies for improving workflow between teachers and students. We can guide you on how to increase efficiency in document flow, and various methods to improve communication beyond the classroom walls using <strong>Google Apps</strong> and additional tools like <strong>G Suite</strong>. Also, ask us questions on how to roll out <strong>Chromebooks</strong> and manage apps, extensions, print from the cloud and administer multiple devices.</p>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <img src="assets/popular-topics-microsoft-icon.jpg" alt="" />
+            <p>Change the way you plan, teach, and assess your students with Microsoft’s hybrid desktop and online platform. Our specialist are ready to help you use this collaborative tool to organize email, personalize websites, and access <strong>One Drive</strong> and <strong>Office</strong> programs from any device.</p>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <img src="assets/popular-topics-smart-icon.jpg" alt="" />
+            <p><strong>SMART Learning Suite</strong> combines lesson delivery, assessment, student collaboration and game-based learning software into one ultimate education suite. We can assist you with connecting students, teachers and devices and turning lessons into enriching learning experiences. Our specialist will answer any question on how to integrate your <strong>SMART Board</strong> and its software into daily classroom instruction.</p>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <img src="assets/popular-topics-apple-icon.jpg" alt="" />
+            <p>Our specialists will guide you on how to effectively integrate tablets into classroom instruction. Starting with the basic usability to increase workflow and classroom management, we also can help with more advanced features of using <strong>iPads</strong> for curriculum based content. Ask us about a variety of apps that focus on educational practice, including Apple’s suite of productivity apps - <strong>Pages</strong>, <strong>Keynote</strong>, and <strong>Numbers</strong>.</p>
+          </div>
+        </div>
+
+        <div class="row padding-top padding-bottom">
+          <div class="col">
+            <p><strong>Our Professional Development Specialists are also prepared to assist with your technology integration into the classroom on content specific examples. Using the technology that you have they will help integrate it into Math, ELA, Science, Social Studies and more, providing Lesson Examples and related resources that fuse technology and learning.</strong></p>
+          </div>
+        </div>
+
+        </div>
+      </div>
+    </section>
+
+  </main>
+
+<?php include 'footer-certifications-bar.php'; ?>
