d3cc4633b504a77c92f94a85aa36ce074766d055
JaySoftye <JaySoftye@iMac.local>
2017-09-14 17:25:40 -0400

    Online pd - down page update

---
+++ onlinepd-index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<title>Teq Professional Development Online</title>
+        <link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Roboto:400,300,500,700,900,100,100italic,300italic,400italic,500italic,700italic,900italic" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/jquery-ui.min.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/jqueryui-theme/jquery-ui.theme.min.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/jquery-ui-timepicker.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/jquery.comiseo.daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/bootstrap-multiselect.css">
+<link rel="stylesheet" type="text/css" href="http://onlinepd.teq.com/css/styles.css?dt=1505422526">
+
+<script type="text/javascript" src="https://f.vimeocdn.com/js/froogaloop2.min.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/jquery.min.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/jquery-ui.min.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/jquery-ui-timepicker.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/moment.min.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/jquery.comiseo.daterangepicker.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/bootstrap-multiselect.js"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/video-settings/javascript?dt=1505422526"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/application.js?dt=1505422526"></script>
+<script type="text/javascript" src="http://onlinepd.teq.com/js/lib/jquery.cookie.js"></script>
+</head>
+<body id="front-end" class="index-index">
+  <div class="wide-container">
+     <div class="row"><img src="http://www-dev.teq.com/_opd3-foundationtemp/assets/online-pd-down-opd-launch.svg" /></div>
+   </div>
+   <div class="main-content" style="background-color: white; padding: 10rem 0;">
+   <div class="header container">
+      <div class="row">
+        <div class="col-xs-6">
+          <div class="logo-box">
+            <a href="/" class="logo"><img src="http://onlinepd.teq.com/images/logo.png" alt="TEQ Online Professional Development"></a>
+            <div class="slogan">
+              <span>Professional Development</span> The Fusion of Technology and Learning
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h3>In preparation for the launch of our new Online PD Platform, <u>OnlinePD.teq.com</u> will <strong>not be available from 2pm EST, Friday 9/15 until the morning of Monday 9/18.</h3>
+        </div>
+      </div>
+    </div>
+    </div>
+
+   <div class="wide-container footer-container">
+      <div class="container-fluid footer">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="contact-info white">
+                <p>Questions? Request a feature. Report a bug.</p>
+                <p class="email"><a href="#">onlinepd@teq.com</a></p>
+                <p class="phone">877.455.9369</p>
+                <p class="copyright">Copyright © Teq <a href="/pages/privacy">Privacy Policy</a>, <a href="/pages/terms">Terms</a></p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="sponsor-img"><img src="/images/img-earn-our.jpg" alt=""></div>
+            </div>
+            <div class="col-md-4">
+              <div class="social-networks">
+                <h3>Follow Us</h3>
+                <ul>
+                  <li><a class="facebook-btn" href="https://www.facebook.com/myTeq/" target="_blank">Facebook</a></li>
+                  <li><a class="twitter-btn" href="https://twitter.com/TeqPD" target="_blank">Twitter</a></li>
+                  <li><a class="google-plus-btn" href="https://plus.google.com/100123653505446384048" target="_blank">Google+</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</body>
+</html>
