af00004887d6e5e26b4fd36734b35f5860ebef1a
JaySoftye <JaySoftye@iMac.local>
2017-09-25 14:55:57 -0400

    Upcoming Courses Page updates

--- profile-pd-plan-upcoming.php
+++ profile-pd-plan-upcoming.php
@@ -28,7 +28,7 @@
       </div>
     </section>
 
-    <section class="container-fluid padding-top padding-bottom">
+    <section class="container-fluid light-gray-bg padding-top padding-bottom">
       <div class="container">
 
         <div class="row">
@@ -44,7 +44,54 @@
           </div>
         </div>
 
-          <div class="row row-eq-height padding-top padding-bottom">
+        <div class="row padding-top padding-bottom">
+          <div class="col">
+            <h4 class="salmon-text"><strong>We’re sorry.</strong></h4>
+            <p class="salmon-text">You do not appear to be registered for any upcoming courses.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <article class="col-8">
+            <div class="card">
+              <div class="card-header">
+                <ul class="nav">
+                  <li class="nav-item">
+                    <li class="nav-item">
+                      <p class="lead white">NEXT AVAILABLE COURSE IS:</p>
+                    </li>
+                  </li>
+                  <li class="nav-item">
+                    <?php include 'category-details-admin-menu.php'; ?>
+                  </li>
+                </ul>
+              </div>
+              <div class="card-block">
+                <h5 class="card-title"><a href="#">Teq DTC: Microsoft Office 365 Session 3- Planner and Outlook</a></h5>
+                <p>Session 3 of our Microsoft Digital Teacher Certification focuses on creating a project timeline, increasing communication, and organizing meetings with the use of Microsoft Planner and Outlook. Within Planner, we will guide you through the process of assigning and organizing tasks to keep track of progress and assign jobs to keep responsibilities clear. Using Outlook, we’ll view group member availability, create meeting appointments while finding appropriate locations and times, as well as give you some beneficial tips and tricks for staying connected through emails.</p>
+              </div>
+              <div class="card-footer"><span class="course-details-date">Tuesday, May 26, 2015 @ 3:00PM EST</span><button class="btn gray float-right">Register for this course</button></div>
+            </div>
+          </article>
+        </div>
+
+        <div class="row padding-bottom">
+          <div class="col">
+            <h3>You can also visit the <a href="http://opd.teq.com/events"  class="teq-blue-text"><u>Course Calendar</u></a> for all our new and upcoming courses!</h3>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <section class="container-fluid">
+      <div class="container">
+
+        <div class="row padding-top padding-bottom">
+          <div class="col"><h3><strong>Similar courses you may be interested in:</strong></h3></div>
+        </div>
+
+          <div class="row row-eq-height padding-bottom">
 
             <article class="col-md-4">
               <div class="card">
