780cd4f085b7e07a72e51598582a17330c39c501
JaySoftye <JaySoftye@iMac.local>
2017-11-09 13:21:01 -0500

    updated FAQ section

--- faqs.php
+++ faqs.php
@@ -28,7 +28,7 @@
         </div>
       </div>
 
-      <div class="row padding-top faq-list">
+      <div class="row padding-top padding-bottom faq-list">
         <ul>
           <li class="faq">
             <a class="question">How do I receive my Certificate after viewing a course?</a>
@@ -82,6 +82,89 @@
             <a class="question">Where do I find courses recommended to me by my school?</a>
             <div class="collapse" id="coursesrecommended">
               <span class="answer">If a playlist has been created and shared by your School or Districts administrators you can find it in your "My PD Plan". <a href="https://onlinepd.teq.com/users/plan/recommended">https://onlinepd.teq.com/users/plan/recommended</a></span>
+            </div>
+          </li>
+          <li class="faq">
+            <a class="question">How do I create a Free account?</a>
+            <div class="collapse" id="freeaccount">
+              <span class="answer">You can create a free account by visiting opd.teq.com/getting started or by clicking sign up as a basic user on the pricing page <a href="https://onlinepd.teq.com/pricing">https://onlinepd.teq.com/pricing</a></span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">How do I create an account with a group code provided by my School or District?</a>
+            <div class="collapse" id="groupcode">
+              <span class="answer">You can create an account with a group code by visiting <a href="https://onlinepd.teq.com/users/register/group">https://onlinepd.teq.com/users/register/group</a></span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">When I create an account it says "Email in System", how can I access my account?</a>
+            <div class="collapse" id="emailsystem">
+              <span class="answer">This means you already have an account, use the "Forgot Password" option or email us at onlinepd@teq.com to help you access your account.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">How do I access Free Content?</a>
+            <div class="collapse" id="freecontent">
+              <span class="answer">Once you create a free account at opd.teq.com/gettingstarted you will be brought to a page with our most popular free content. Or if you sign up as a basic user  at <a href="https://onlinepd.teq.com/pricing">https://onlinepd.teq.com/pricing</a> you can also visit the "Course Library" and filter the "Type" to "Free" you can see all of our Free content.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">How do I know what my account has access to?</a>
+            <div class="collapse" id="accountaccess">
+              <span class="answer">You can access your account info by clicking on “My Profile” and selecting the “My Account” link</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">How do I contact support?</a>
+            <div class="collapse" id="contactsupport">
+              <span class="answer">Use the red nOw Instructional Support option on the right side of the page to connect with our team.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">Why won't my Live Video nOw Instructional Support work?</a>
+            <div class="collapse" id="livenow">
+              <span class="answer">The video call requires an enabled microphone and web camera. It will also only work on Google Chrome and Mozilla Firefox. All other support options work on all web browsers and devices.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">What are the hours for nOw Instructional Support?</a>
+            <div class="collapse" id="nowhours">
+              <span class="answer">We are available from 8am-5pm Eastern Time.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">Why does the video keep freezing as I play it?</a>
+            <div class="collapse" id="videofreezing">
+              <span class="answer">Check with your network administrator and test your available bandwidth.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">When I completed the quiz at the end of the course, nothing happened. What should I do?</a>
+            <div class="collapse" id="completednothing">
+              <span class="answer">First, Check to see if the certificate was generated in the "Completed" section of your "My Usage. If not, use the red nOw Instructional Support option on the right side of the page to connect with our team.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">When I completed a course the quiz never appeared and the course made me start from the beginning. What should I do?</a>
+            <div class="collapse" id="quiznever">
+              <span class="answer">Try to use the latest browser versions available, check with your network administrator and test your available bandwidth. Or Use the red nOw Instructional Support option on the right side of the page  to connect with our team. We will assist you with the issue and make sure you receive the certificate for the course.</span>
+            </div>
+          </li>
+
+          <li class="faq">
+            <a class="question">When I play a video it says the video is blocked or will not play.</a>
+            <div class="collapse" id="videoblocked">
+              <span class="answer">It is possible that your school's network has blocked our video player. In order to view archived courses you must be able to access videos from Flowplayer.com. We can speak with your network admin if you need to connect us.</span>
             </div>
           </li>
         </ul>
