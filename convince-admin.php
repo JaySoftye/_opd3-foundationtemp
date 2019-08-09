9b68b73bcaa5ad48e397ca6682a97b39dd76df34
JaySoftye <jonathansoftye@teq.com>
2019-08-09 10:13:43 -0400

    Create convince-admin.php

---
+++ convince-admin.php
@@ -0,0 +1,310 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Convince your administrator you need Opd</title>
+    <!-- Roboto -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900" rel="stylesheet" type="text/css">
+    <!-- Compiled Version of Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/main-styles.css">
+
+<script src="https://cdn.jsdelivr.net/lodash/4.17.4/lodash.min.js" integrity="sha256-IyWBFJYclFY8Pn32bwWdSHmV4B9M5mby5bhPHEmeY8w=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="//releases.flowplayer.org/7.1.2/skin/skin.css">
+<script src="http://releases.flowplayer.org/7.1.1/commercial/flowplayer.min.js"></script>
+<script src="http://releases.flowplayer.org/hlsjs/flowplayer.hlsjs.min.js"></script>
+
+<style>
+  video {width: 100%;}
+  section {position: relative; width: 100%;}
+  section#convince-admin {position: fixed; z-index: -1;}
+  .convince-admin-text {color: rgb(255, 118, 125);}
+  .convince-admin-grey-bg {background-color: rgb(236, 238, 239);}
+  a.convince-btn {position: relative; display: inline-block; z-index: 5; padding: 10px 30px; margin-left: 10px; margin-right: 10px; background-color: rgb(255, 118, 125); border-radius: 50px; -webkit-border-radius: 50px; -moz-border-radius: 50px; outline: none; text-align: center; font-weight: 700; color: white;}
+  .convince-admin-header {padding-top: 2.625rem; padding-bottom: 2.875rem; background-color: rgb(255, 118, 125);}
+  .convince-admin-header h1 {font-size: 3.5rem; font-weight: 700; color: white;}
+  .convince-admin-header h2 {font-size: 2.25rem; font-weight: 700; color: white;}
+  .convince-admin-footer {padding-top: 2.5rem; padding-bottom: 1.375rem; background-color: rgb(236, 238, 239);}
+  .convince-admin-body {position: absolute; padding-right: 0; padding-left: 0; margin-top: 660px; background: white;}
+  img.user-image {position: absolute; right: -5%; bottom: 0; width: auto; z-index: 0;}
+  h3 {position: relative; right: 10rem; opacity: 0; padding-left: 36px; background: transparent url('assets/convince-admin-h3-bg.svg') no-repeat left center; background-size: contain; color: rgb(127, 168, 224);}
+  div.slide-left {position: relative; left: 15rem; opacity: 0; }
+  blockquote {padding: 1.675rem; margin-bottom: 0;}
+  blockquote p {margin-bottom: .125rem; font-size: 1.875rem; color: rgb(127, 168, 224);}
+  div.csat {min-height: 75vh; padding: 0 0; margin-top: 5.375rem; margin-bottom: 5.375rem; background: transparent url('assets/convince-admin-csat-bg.jpg') no-repeat center center; background-size: cover;}
+  .large-stat {font-size: 9.375rem;}
+  .stat-text {margin-top: -2.5rem; font-size: 4.125rem;}
+  table.table {font-size: .8rem; color: rgb(153, 164, 174);}
+  div#opd-school-hours {padding-left: 1.675rem;}
+  div#opd-school-hours table.table td {padding: .375rem;}
+  div.convince-form {padding-top: 5.875rem; padding-bottom: 5.875rem; margin-bottom: 5.875rem; background-color: rgb(64, 85, 117);}
+  div.convince-form div.form-container {padding: 1.375rem 1.5rem; background-color: rgb(119, 134, 157);}
+  .hs-richtext h5 {margin-top: 25px; margin-bottom: 0;}
+  span.hs-form-required {display: none !important;}
+  .hs-form-field.field {margin-bottom: 5px !important;}
+  fieldset {max-width: 100% !important;}
+  input.hs-input, textarea.hs-input, select.hs-input {width: 92.5% !important; height: 24 !important; padding: 10px !important; font-family:'Roboto',Helvetica,Arial,san-serif; font-size: 1rem !important; font-weight: normal; color: #666; border: none !important; -webkit-border-radius: 0 !important; -moz-border-radius:0 !important; -ms-border-radius: 0 !important; border-radius: 0 !important; font-weight: 300 !important;}
+  .hs-button {background-color: rgb(255, 118, 125) !important;  background-image: none !important; padding: 15px 30px !important; text-shadow: none !important; border-top: none !important; border-bottom: none !important; border-right: none !important; border-left: none !important; border-radius: 50px !important; -webkit-border-radius: 50px !important; -moz-border-radius: 50px !important; outline: none; text-align: center !important; font-size: 1rem !important; font-weight: 700 !important;}
+  ul.hs-error-msgs.inputs-list li label {color: rgb(255, 118, 125);}
+  @media screen and (max-width: 680px) {
+    h3 {background-image: none;}
+    section#convince-admin  {position: relative; z-index: -1;}
+    img.user-image {width: 50%;}
+    .convince-admin-body {margin-top: 0px;}
+  }
+</style>
+
+  </head>
+<body>
+  <main>
+    <div class="modal fade" id="modalLove" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+            <video controls="controls">
+              <source src="https://cf561eb26.lwcdn.com/v-7ae1507a-20b1-4b55-ab30-27f4b0f83001.mp4" type="video/mp4" />Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <section id="convince-admin">
+      <div class="container-fluid convince-admin-header">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-2 col-sm-2">
+              <p><a style="opacity: 0.5;" href=""><strong>&lt;-- Back</strong></a></p>
+              <img class="padding-top" src="assets/convince-admin-opd-logo.svg" alt="" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-7 col-sm-7">
+              <h1 class="display-4">Everything you need</h1>
+              <h2>to convince your administrator <br />they need Teq Online PD(Opd).</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid convince-admin-footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 col-sm-7">
+              <p><strong>Download this handy booklet</strong> <a class="convince-btn" target="_blank" href="assets/OPD-Deployment-Booklet_Single-Users_72dpi.pdf">the mic drop</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="container">
+        <img class="user-image" src="assets/convince-admin-happy-user.png" alt="" />
+      </section>
+    </section>
+
+    <section class="container-fluid convince-admin-body padding-top">
+
+      <div class="container padding-top">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <img src="assets/convince-admin-west-islip.jpg" alt=""  data-toggle="modal" data-target="#modalLove" />
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <h3><strong>See how your peers are utilizing Opd</strong></h3>
+            <div class="d-flex justify-content-center padding-top slide-left">
+              <blockquote class="col-10 convince-admin-grey-bg">
+                <p>“Flexibility for teachers to take courses whenever they want”</p>
+                <footer>Trish Hinchman<br />Technology Integration Specialist<br />West Islip School District</footer>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container padding-bottom">
+        <div class="row padding-top">
+          <div class="col-md-6 col-sm-6">
+            <p><a class="convince-btn" href="#modalLove" data-toggle="modal" data-target="#modalLove">See the love</a> West Islip School District Testimonial</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid csat d-flex align-items-center">
+        <div class="container padding-top padding-bottom">
+          <div class="row">
+            <div class="col-md-6">
+              <h3><strong>See how your peers are rating Opd</strong></h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 align-self-center slide-left">
+              <div class="row padding">
+                <h2 class="col-10 large-stat convince-admin-text"><strong>97.5<sup>%</sup></strong></h2>
+                <h4 class="col-10 stat-text convince-admin-text"><strong>Customer Satisfaction Rating</strong></h4>
+              </div>
+            </div>
+            <div class="col-md-6 align-self-center">
+              <h5><img src="assets/convince-admin-oh-la-la.svg" alt="" /></h5>
+              <div class="row d-flex justify-content-center">
+                <h5 class="col-10 white padding-bottom">Teq Opd surveys each participant to determine the overall Customer Satisfaction (CSAT) with the content of each PD course. The CSAT results are analyzed and shared with the school administration in order to validate the Return on Investment (ROI).</h5>
+                <h5 class="col-10 convince-admin-text">We have been tracking our CSAT results for 5 years and have consistently achieved a rating of 96.7% or above.</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row padding-top">
+            <div class="col-md-12">
+              <h3><strong>Picture yourself in this group of edtech pioneers</strong></h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section>
+        <div class="container">
+          <div class="row padding-bottom">
+            <div class="col-md-5 col-sm-5">
+              <div class="d-flex justify-content-center slide-left">
+                <blockquote>
+                  <p>Your colleagues are completing thousands of professional development hours each month using Opd.</p>
+                </blockquote>
+              </div>
+              <div id="opd-school-hours">
+                <table class="table"><tbody>
+                  <tr><td>525K</td><td>Edward R. Murrow High School</td><td>Brooklyn</td></tr>
+                  <tr><td>180K</td><td>The SEEALL Academy</td><td>Brooklyn</td></tr>
+                  <tr><td>75Q</td><td>Robert E. Peary School</td><td>Queens</td></tr>
+                  <tr><td>185Q</td><td>Edward Bleeker</td><td>Queens</td></tr>
+                  <tr><td>445R</td><td>Port Richmond High School</td><td>Staten Island</td></tr>
+                  <tr><td></td><td><strong>and many many more...</strong></td><td></td></tr>
+                </tbody></table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid d-flex align-items-center convince-admin-header">
+          <div class="container">
+            <div class="row align-items-center padding">
+              <div class="col-md-2 col-sm-2 slide-left">
+                <h4 class="white"><strong class="stat-text">3,016</strong></h4>
+              </div>
+              <div class="col-md-3 col-sm-3 slide-left">
+                <h4 class="white">Schools using Opd</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+        <section class="container">
+          <img class="user-image" src="assets/convince-admin-schools-using-opd.png" alt="" />
+        </section>
+      </section>
+      <div class="container-fluid convince-form">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h4 class="white"><strong>Don’t have the time to convince them?</strong></h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 form-container">
+              <p><strong>Fill out this form and we will let your administrator know you are interested, and how awesome they would be if they bought this for you!</strong></p>
+<!--[if lte IE 8]>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+<![endif]-->
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+<script>
+  hbspt.forms.create({
+    portalId: "182596",
+    formId: "87d3432b-feb9-478c-82e4-27185b1a43b1"
+  });
+</script>
+            </div>
+            <div class="col-md-6">
+              <div class="d-flex justify-content-center">
+                <blockquote class="col-10">
+                  <h3><strong>Help us help you</strong></h3>
+                  <div class="padding slide-left">
+                    <p class="convince-admin-text"><a class="convince-admin-text" target="_blank" href="assets/OPD-Deployment-Booklet_Single-Users_72dpi.pdf"><u>Download this handy booklet</u></a> to convince your administrator to purchase Opd for you and your colleagues.</p>
+                    <footer class="padding-top"><a class="convince-btn" target="_blank" href="assets/OPD-Deployment-Booklet_Single-Users_72dpi.pdf">Download Booklet</a></footer>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid d-flex align-items-center padding-top padding-bottom">
+        <div class="container">
+          <div class="row padding-bottom">
+            <div class="col-md-6 align-self-center">
+              <h3><strong>A few helpful links</strong></h3>
+              <div class="padding slide-left">
+                <h5><a class="convince-admin-text" target="_blank" href="http://www.teq.com/about-us/testimonials/">http://www.teq.com/about-us/testimonials/</a></h5>
+                <h5><a class="convince-admin-text" target="_blank" href="http://www.teq.com/pd-bios/">http://www.teq.com/pd-bios/</a></h5>
+                <h5><a class="convince-admin-text" target="_blank" href="https://opd.teq.com/events">https://opd.teq.com/events</a></h5>
+                <h5><a class="convince-admin-text" target="_blank" href="https://onlinepd.teq.com/courses">https://onlinepd.teq.com/courses</a></h5>
+                <h5><a class="convince-admin-text" target="_blank" href="http://www.teq.com/pdf/online-pd-2017copy.pdf">Online PD (Opd) Brochure</a></h5>
+              </div>
+            </div>
+            <div class="col-md-6 align-self-center">
+              <div class="d-flex justify-content-center">
+                <blockquote class="col-10">
+                  <h1 class="display-4"><strong>Thanks and have an amazing day!</strong></h1>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="container-fluid light-gray-bg">
+        <footer class="container padding-top">
+          <div class="row">
+            <div class="col-md-12">
+              <p><small>Opd&#174; | 877.455.9369</small></p>
+            </div>
+          </div>
+        </footer>
+      </section>
+
+    </section>
+
+  </main>
+<script>
+$(document).ready(function(){
+
+  $('.modal').on('click', function () {
+      // Just go ahead and pause/reset all the video elements
+      $('.modal-body video').get(0).pause();
+  });
+
+  $(window).scroll( function(){
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    $('h3').each( function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+       if( bottom_of_window >= bottom_of_object ){
+        $(this).animate({'opacity':'1', 'right':'0' },350);
+       }
+    });
+    $('div.slide-left').each( function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+       if( bottom_of_window >= bottom_of_object ){
+        $(this).animate({'opacity':'1', 'left':'0' },550);
+       }
+    });
+
+  });
+});
+
+</script>
+</body>
+</html>
